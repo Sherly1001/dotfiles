3cf55d201b2956e6d72f080c5cfe767633255cdf
Sherly1001 <Sherly1001@users.noreply.github.com>
2025-05-02 16:58:15 +0900

    fzf: remove default opts

--- .config/fish/settings/fzf.fish
+++ .config/fish/settings/fzf.fish
@@ -4,13 +4,6 @@ not type -q fzf && exit 0
 
 fzf --fish | source
 
-set -gx FZF_DEFAULT_OPTS "
-  --bind=∂:kill-word
-  --bind=˚:kill-line
-  --bind=∫:backward-word
-  --bind=ƒ:forward-word
-"
-
 set -gx FZF_DEFAULT_COMMAND "
   rg -uu -H --color=never --files
 "
