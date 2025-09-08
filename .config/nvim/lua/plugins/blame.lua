ac38c15c3a8d0ad4517ce0caddc2fe427d7164cd
Sherly1001 <Sherly1001@users.noreply.github.com>
2025-09-08 10:42:23 +0900

    nvim: add file blame

--- .config/nvim/lua/plugins/blame.lua
+++ .config/nvim/lua/plugins/blame.lua
@@ -1,9 +1,16 @@
 return {
-  "f-person/git-blame.nvim",
-  event = "LazyFile",
-  opts = {
-    enabled = true,
-    message_template = " <author> • <date> • <summary> • <<sha>>",
-    date_format = "%r",
+  {
+    "FabijanZulj/blame.nvim",
+    event = "LazyFile",
+    opts = {},
+  },
+  {
+    "f-person/git-blame.nvim",
+    event = "LazyFile",
+    opts = {
+      enabled = true,
+      message_template = " <author> • <date> • <summary> • <<sha>>",
+      date_format = "%r",
+    },
   },
 }
