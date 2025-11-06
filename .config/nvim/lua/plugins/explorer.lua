20c83141041dbb7201aaafa0a374101ea8d8abdc
Sherly1001 <Sherly1001@users.noreply.github.com>
2025-11-07 00:12:35 +0900

    nvim: use snacks.picker instead of fzf-lua

--- .config/nvim/lua/plugins/explorer.lua
+++ .config/nvim/lua/plugins/explorer.lua
@@ -1,62 +1,10 @@
 return {
   {
-    "ibhagwan/fzf-lua",
-  },
-  {
     "folke/snacks.nvim",
     opts = {
       scroll = { enabled = false },
       indent = {
         animate = { enabled = false },
-      },
-    },
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      popup_border_style = "rounded",
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          hide_hidden = false,
-        },
-      },
-      default_component_configs = {
-        modified = { symbol = "●" },
-        git_status = {
-          symbols = {
-            -- Change type
-            added = "✚",
-            modified = "●",
-            deleted = "✖",
-            renamed = "󰁕",
-            -- Status type
-            untracked = "",
-            ignored = "",
-            unstaged = "󰄱",
-            staged = "",
-            conflict = "",
-          },
-        },
-      },
-      event_handlers = {
-        {
-          event = "file_open_requested",
-          handler = function()
-            require("neo-tree.command").execute({ action = "close" })
-          end,
-        },
-        {
-          event = "neo_tree_buffer_enter",
-          handler = function()
-            vim.cmd([[
-              setlocal number
-              setlocal relativenumber
-            ]])
-          end,
-        },
       },
     },
   },
