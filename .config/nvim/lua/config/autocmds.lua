976fb5d499c6d81dfc2a6cfa0cebc02a890467fb
Sherly1001 <Sherly1001@users.noreply.github.com>
2025-11-06 15:41:55 +0900

    nvim: add copilot

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -36,6 +36,20 @@ vim.api.nvim_create_autocmd("CmdlineLeave", {
   end,
 })
 
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuOpen",
+  callback = function()
+    vim.b.copilot_suggestion_hidden = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuClose",
+  callback = function()
+    vim.b.copilot_suggestion_hidden = false
+  end,
+})
+
 vim.api.nvim_create_autocmd("VimLeave", {
   group = vim.api.nvim_create_augroup("RestoreCursorShape", { clear = true }),
   command = "set guicursor=a:ver100-blinkwait300-blinkon200-blinkoff150",
