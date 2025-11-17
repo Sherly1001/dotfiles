d9a6e17ef2af04ff69c998100b810b13335a5cf5
Sherly1001 <Sherly1001@users.noreply.github.com>
2025-11-17 15:48:38 +0900

    nvim: update lualine

--- .config/nvim/lua/plugins/lines.lua
+++ .config/nvim/lua/plugins/lines.lua
@@ -22,8 +22,7 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
       opts.sections.lualine_z = opts.sections.lualine_y
-      opts.sections.lualine_y = opts.sections.lualine_x
-      opts.sections.lualine_x = {}
+      opts.sections.lualine_y = {}
     end,
   },
 }
