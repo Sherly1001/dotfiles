1418305982103d44013c1745e54d3f75f169e93b
Sherly1001 <Sherly1001@users.noreply.github.com>
2026-04-05 02:17:45 +0900

    fish: fix wrong status code when re-source config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,16 +3,16 @@
 set fish_greeting
 
 set -l fish_theme ~/.config/fish/themes/tokyonight_moon.fish
-test -f $fish_theme && source $fish_theme
+test -f $fish_theme && source $fish_theme || true
 
 set -l fish_setting ~/.config/fish/settings
 set -l fish_local_after "$fish_setting/local-after.fish"
 set -l fish_local_before "$fish_setting/local-before.fish"
 
-test -f $fish_local_before && source $fish_local_before
+test -f $fish_local_before && source $fish_local_before || true
 
 source "$fish_setting/alias.fish"
 source "$fish_setting/funcs.fish"
 source "$fish_setting/fzf.fish"
 
-test -f $fish_local_after && source $fish_local_after
+test -f $fish_local_after && source $fish_local_after || true
